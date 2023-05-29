c601a6339931e3fd1d7ebcd476651193c517ff90
DmytroBozhor <[dimaastop1@gmail.com]>
2023-05-29 20:42:57 +0200

    homework 16

---
+++ src/main/java/ua/ItHilell/homework16/SortingAlgorithms.java
@@ -0,0 +1,59 @@
+package ua.ItHilell.homework16;
+
+public class SortingAlgorithms {
+
+    public static void quickSort(int arr[], int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+    }
+
+    static private int partition(int arr[], int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+        int swapTemp = arr[i + 1];
+        arr[i + 1] = arr[end];
+        arr[end] = swapTemp;
+        return i + 1;
+    }
+
+    public static void cocktailSort(int nums[]) {
+        boolean swapped;
+        do {
+            swapped = false;
+            for (int i = 0; i <= nums.length - 2; i++) {
+                if (nums[i] > nums[i + 1]) {
+                    //test if two elements are in the wrong order
+                    int temp = nums[i];
+                    nums[i] = nums[i + 1];
+                    nums[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+            swapped = false;
+            for (int i = nums.length - 2; i >= 0; i--) {
+                if (nums[i] > nums[i + 1]) {
+                    int temp = nums[i];
+                    nums[i] = nums[i + 1];
+                    nums[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+        } while (swapped);
+    }
+
+}
